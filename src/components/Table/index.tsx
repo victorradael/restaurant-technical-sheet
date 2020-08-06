496565124664365f9a547de4cf29ae68a8ef53a5
Victor Radael <victorradael@hotmail.com>
2020-08-06 20:25:33 -0300

    :rocket: For

--- src/components/Table/index.tsx
+++ src/components/Table/index.tsx
@@ -17,31 +17,39 @@ const Table = () => {
 
   return (
     <Container>
-      <table>
-        <tr>
-          <th>INGREDIENTES</th>
-          <th>QTDE. LIQ</th>
-          <th>UNIDADE</th>
-          <th>Rendimento %</th>
-          <th>Fator de Correcao</th>
-          <th>QTDE BRUTA</th>
-          <th>CUSTO BRUTO UNIT.</th>
-          <th>CUSTO TOTAL</th>
-        </tr>
-        {datasheetInfo &&
-          datasheetInfo.map((ingredient) => (
-            <tr>
-              <td>{ingredient.nome}</td>
-              <td>{ingredient.quantidadeLiquida}</td>
-              <td>{ingredient.unidade}</td>
-              <td>{ingredient.rendimento}</td>
-              <td>{ingredient.fatorDeCorrecao}</td>
-              <td>{ingredient.quantidadeBruta}</td>
-              <td>{ingredient.custoBruto}</td>
-              <td>{ingredient.custoTotal}</td>
-            </tr>
-          ))}
-      </table>
+      <section>
+        <form>
+          <label htmlFor="ingredient">Ingrediente</label>
+          <input type="text" name="ingredient" id="" />
+        </form>
+      </section>
+      <section>
+        <table>
+          <tr>
+            <th>INGREDIENTES</th>
+            <th>QTDE. LIQ</th>
+            <th>UNIDADE</th>
+            <th>Rendimento %</th>
+            <th>Fator de Correcao</th>
+            <th>QTDE BRUTA</th>
+            <th>CUSTO BRUTO UNIT.</th>
+            <th>CUSTO TOTAL</th>
+          </tr>
+          {datasheetInfo &&
+            datasheetInfo.map((ingredient) => (
+              <tr>
+                <td>{ingredient.nome}</td>
+                <td>{ingredient.quantidadeLiquida}</td>
+                <td>{ingredient.unidade}</td>
+                <td>{ingredient.rendimento}</td>
+                <td>{ingredient.fatorDeCorrecao}</td>
+                <td>{ingredient.quantidadeBruta}</td>
+                <td>{ingredient.custoBruto}</td>
+                <td>{ingredient.custoTotal}</td>
+              </tr>
+            ))}
+        </table>
+      </section>
     </Container>
   );
 };
